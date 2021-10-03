223cb176268fb9c78e91875c7a77b3423585d74e
ryan.matos <ryan.matos@itixti.com.br>
2021-10-03 14:50:48 -0300

    fix: Routes

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,15 @@ import './database/connection'
 import routes from './routes'
 
 const app = express()
+const cors = require('cors')
+
+var corsOptions = {
+   origin: '*',
+   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+ }
 
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use(routes)
 
 app.listen(3333, () => console.log('listening port 3333'))
